aefaa6352587c81d2c164aacf76f369ecaa6c917
Jelle De Laender <info@codingmammoth.com>
2012-08-06 10:49:08 +0200

    Fixed iOS popover 'volume-change-notification'

--- CMVolumeSlider.m
+++ CMVolumeSlider.m
@@ -2,17 +2,19 @@
 //  CMVolumeSlider.m
 //
 //  Created by Jelle De Laender on 20/05/12.
+//  Updated by Jelle De Laender on 06/08/12.
 //
 //  https://github.com/CodingMammoth/CMVolumeSlider
 //
 
 #import "CMVolumeSlider.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @implementation CMVolumeSlider
 
--(id)initWithCoder:(NSCoder *)aDecoder
+- (id)initWithCoder:(NSCoder*)coder
 {
-    self = [super initWithCoder:aDecoder];
+    self = [super initWithCoder:coder];
     if (self)
     {
         [[NSNotificationCenter defaultCenter] addObserver:self
@@ -26,40 +28,57 @@
         // current value
         [self systemVolumeChanged:nil];
         [self setBackgroundColor:[UIColor clearColor]];
+        
+        // Fix to hide volume popover
+        MPVolumeView *volumeView = [[[MPVolumeView alloc] initWithFrame:
+                                     CGRectMake(-500, -500, 10, 10)] autorelease];
+        [self addSubview:volumeView];
     }
+    
     return self;
 }
+
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
-    if (self) {
+    if (self)
+    {
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(systemVolumeChanged:)
                                                      name:@"AVSystemController_SystemVolumeDidChangeNotification"
                                                    object:nil];
         [self setMaximumValue:1.0];
         [self setMinimumValue:0.0];
-
+        
         // current value
         [self systemVolumeChanged:nil];
         [self setBackgroundColor:[UIColor clearColor]];
+        
+        // Fix to hide volume popover
+        MPVolumeView *volumeView = [[[MPVolumeView alloc] initWithFrame:
+                                     CGRectMake(-500, -500, 10, 10)] autorelease];
+        [self addSubview:volumeView];
     }
+    
     return self;
 }
 
--(void)systemVolumeChanged:(NSNotification*)aNotification
+
+- (void)systemVolumeChanged:(NSNotification*)aNotification
 {
     [self setValue:[MPMusicPlayerController applicationMusicPlayer].volume];
 }
 
+
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [super touchesMoved:touches withEvent:event];
-    [[MPMusicPlayerController applicationMusicPlayer] setVolume:self.value];    
+    [[MPMusicPlayerController applicationMusicPlayer] setVolume:self.value];
 }
 
--(void)dealloc
+
+- (void)dealloc
 {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
     [super dealloc];
